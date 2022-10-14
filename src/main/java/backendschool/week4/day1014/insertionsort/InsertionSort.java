33a8470cb79c214a07b29d4c45575d0161d378b3
KimJaeGeun <jgk9612@gmail.com>
2022-10-14 10:25:23 +0900

    refactor: Add loop i parameter

--- src/main/java/backendschool/week4/day1014/insertionsort/InsertionSort.java
+++ src/main/java/backendschool/week4/day1014/insertionsort/InsertionSort.java
@@ -3,18 +3,22 @@ package backendschool.week4.day1014.insertionsort;
 import java.util.Arrays;
 
 public class InsertionSort {
-    public int[] sort(int[] arr, int i) {
+    public int[] sort(int[] arr) {
         /** i = 1 arr[1] vs arr[0]
          * i = 2 arr[2] vs arr[1]
          * i = 2 arr[1] vs arr[0]
          */
-        int j = i;
-        if (arr[j] < arr[j - 1]) {
-            int temp = arr[j - 1];
-            arr[j - 1] = arr[j];
-            arr[j] = temp;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+//                System.out.printf("i:%d j:%d j-1:%d\n", i, j, j - 1);
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
-        return new int[]{};
+    return arr;
     }
 
     public static void main(String[] args) {
@@ -22,7 +26,8 @@ public class InsertionSort {
         int[] arr = {8, 5, 6, 2, 4};
 
         InsertionSort insertionSort = new InsertionSort();
-        int[] result = insertionSort.sort(arr, 1);
+//        arr = new int[]{5, 8, 6, 2, 4};
+        int[] result = insertionSort.sort(arr);
 
         System.out.println(Arrays.toString(result));
     }
