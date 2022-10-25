18f8813d6fb75b4213008eee780cd40dd5c77877
KimJaeGeun <jgk9612@gmail.com>
2022-10-25 09:27:49 +0900

    feat: Add hash/hashTable algorithm

---
+++ src/main/java/algorithm/datastructure/HashTable.java
@@ -0,0 +1,22 @@
+package algorithm.datastructure;
+
+public class HashTable {
+
+    private int size = 10000;
+
+    public HashTable() {
+
+    }
+
+    public HashTable (int size) {
+        this.size = size;
+    }
+
+    public  int hash(String key) {
+        int asciiSum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % size;
+    }
+}
